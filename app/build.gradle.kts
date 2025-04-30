9604f7d8e1fac569fbd2250c593dba9a702f4302
lab_services_student <st10043271@imconnect.edu.za>
2025-05-01 01:27:42 +0200

    Updated files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,4 @@ dependencies {
     implementation("androidx.room:room-ktx:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
 
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
-
 }
