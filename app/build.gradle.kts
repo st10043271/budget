dab773e1ef01716229fef3a069b26b9925c0dc97
lab_services_student <st10043271@imconnect.edu.za>
2025-04-30 22:06:06 +0200

    Added a dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,8 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
+
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+
 }
